efbe89cecb4cbe7c1a4656cd3ee0064eb380c80d
Aether <tifsnunes@gmail.com>
2023-05-18 17:18:41 -0300

    home page creation

---
+++ script.js
@@ -0,0 +1,16 @@
+//console.log(pizzaJson)
+// const c = (el) => document.querySelector(el)
+// const cs = (el) => document.querySelector(el)
+
+pizzaJson.map((item, index) => {
+    //console.log(item)
+    let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
+
+
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+    
+    document.querySelector('.pizza-area').append(pizzaItem)
+})
