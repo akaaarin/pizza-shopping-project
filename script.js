d8c4e435da35d44e2ca822ae85acd6c888535a58
Aether <tifsnunes@gmail.com>
2023-05-19 16:56:16 -0300

    creating click events and animations

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 //console.log(pizzaJson)
-// const c = (el) => document.querySelector(el)
+ const c = (el) => document.querySelector(el)
 // const cs = (el) => document.querySelector(el)
 
 pizzaJson.map((item, index) => {
     //console.log(item)
     let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
 
-
     pizzaItem.querySelector('.pizza-item--img img').src = item.img
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault()
+        
+        c('.pizzaWindowArea').style.opacity = 0
+        c('.pizzaWindowArea').style.display = 'flex'
+        setTimeout(() => {
+         c('.pizzaWindowArea').style.opacity = 1
+        }, 200)
+    }) 
     
     document.querySelector('.pizza-area').append(pizzaItem)
 })
