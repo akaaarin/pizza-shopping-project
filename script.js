a435c5ea96ebb59b754e735563ec518cd6214531
Aether <tifsnunes@gmail.com>
2023-05-22 15:45:48 -0300

    modal events

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let modalQt = 1
  const c = (el)=>document.querySelector(el)
  const cs = (el)=>document.querySelectorAll(el)
 
+ //* LISTAGEM DAS PIZZAS
 pizzaJson.map((item, index) => {
     //console.log(item)
     let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
@@ -25,7 +26,7 @@ pizzaJson.map((item, index) => {
         c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
         c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`
 
-        //c('pizzaInfo--size.selected').classList.remove('selected')
+        c('.pizzaInfo--size.selected').classList.remove('selected')
         
         //tamanhos pizza
         cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
@@ -46,4 +47,34 @@ pizzaJson.map((item, index) => {
     }) 
     
     document.querySelector('.pizza-area').append(pizzaItem)
+})
+
+//* EVENTOS MODAL
+function closeModal(){
+    c('.pizzaWindowArea').style.opacity = 0
+    setTimeout(()=>{
+        c('.pizzaWindowArea').style.display = 'none' 
+    }, 500)
+}
+
+cs('.pizzaInfo--cancelButton', '.pizzaInfo--cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', closeModal)
+})
+
+c('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
+    if(modalQt > 1){
+        modalQt--
+        c('.pizzaInfo--qt').innerHTML = modalQt
+    }
+})
+c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+    modalQt++
+    c('.pizzaInfo--qt').innerHTML = modalQt
+})
+
+cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+    size.addEventListener('click', (e)=>{
+        c('.pizzaInfo--size.selected').classList.remove('selected')
+        size.classList.add('selected')
+    })
 })
